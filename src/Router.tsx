298b69c402a10b0dd58a6eceb5e3c4a544e8a81c
giovani <giovaniocan@gmail.com>
2023-03-06 15:51:46 -0300

    routes created

---
+++ src/Router.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
+import { Checkout } from './pages/checkout'
+import { Home } from './pages/Home'
+import { Success } from './pages/success'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="success" element={<Success />} />
+      </Route>
+    </Routes>
+  )
+}
