839963007dd9900887431cd4a84174fa639f8fc1
giovani <giovaniocan@gmail.com>
2023-03-06 15:21:25 -0300

    default css finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
-    <>
-      <div>teste</div>
-
-      <h1>otatio </h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
