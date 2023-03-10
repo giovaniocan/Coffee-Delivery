f8e478ea2039980378ae209bdf2218643aad3b09
giovani <giovaniocan@gmail.com>
2023-03-09 21:27:48 -0300

    restructuring the checkout component

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,12 +1,45 @@
-import { CartCoffee } from './CartCoffee'
+import { NavLink } from 'react-router-dom'
+import { CardCoffee } from './cardCoffee'
+
 import { FormAddress } from './FormAddress'
-import { CheckoutContainer } from './styles'
+import {
+  CardContainer,
+  CartCoffeeContainer,
+  CheckoutContainer,
+  FinishedButton,
+  SubTotal,
+  Total,
+} from './styles'
 
 export function Checkout() {
   return (
     <CheckoutContainer>
       <FormAddress />
-      <CartCoffee />
+
+      <CartCoffeeContainer>
+        <h2>Faça o seu pedido</h2>
+        <CardContainer>
+          <CardCoffee />
+
+          <SubTotal>
+            <div>
+              <p>Total de itens</p>
+              <h4>R$ 29,70</h4>
+            </div>
+            <div>
+              <p>Entrega</p>
+              <h4>R$ 3,50</h4>
+            </div>
+          </SubTotal>
+          <Total>
+            <p>Total</p>
+            <p>R$ 33,20</p>
+          </Total>
+          <NavLink to="/success" title="checkout">
+            <FinishedButton>confirmar pedido</FinishedButton>
+          </NavLink>
+        </CardContainer>
+      </CartCoffeeContainer>
     </CheckoutContainer>
   )
 }
