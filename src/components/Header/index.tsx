cd5c4cdada87904b28e1a8a579427eda4b110590
giovani <giovaniocan@gmail.com>
2023-03-13 15:46:19 -0300

    cart shows the amount of coffee

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,12 @@ import { HeaderContainer, IconCar, IconMap } from './styles'
 import LogoCoffe from '../../assets/logo.svg'
 import { MapPin, ShoppingCart } from 'phosphor-react'
 import { NavLink } from 'react-router-dom'
+import { useContext } from 'react'
+import { CardCoffeeContext } from '../../context/CardCoffee'
 
 export function Header() {
+  const { cartItems } = useContext(CardCoffeeContext)
+
   return (
     <HeaderContainer>
       <NavLink to="/">
@@ -19,7 +23,7 @@ export function Header() {
           <NavLink to="/checkout" title="checkout">
             <ShoppingCart size={22} color="#C47F17" weight="fill" />
           </NavLink>
-          <p>5</p>
+          <p>{cartItems.length}</p>
         </IconCar>
       </nav>
     </HeaderContainer>
